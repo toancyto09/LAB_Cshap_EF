3cc9d34aca4c6e5ef5cbcf89c2c9e207e9649371
toancyto09 <101265301+toancyto09@users.noreply.github.com>
2024-04-10 12:11:39 +0700

    done lab EF

---
+++ LAB_EF_HuynhVanToan/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_EF_HuynhVanToan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            using (var db = new LAB_EFEntities())
+            {
+                var students = db.Student
+                .Where(s => s.Age >= 18 && s.Age <= 20 && s.Department == "CNTT")
+                .ToList();
+
+                foreach(var s in students)
+                {
+                    Console.WriteLine($"ID: {s.Id}, Name: {s.Name}, Age: {s.Age}, Department: {s.Department}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
